9f4c56302d7cd390f5dc5f98be0c01cc3f167501
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-08-27 12:15:10 +0300

    Header componts done

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoImg from '../assets/quiz-logo.png'
+const Header = () => {
+    return (
+        <header>
+            <img src={ logoImg } alt="Quiz Logo" />
+            <h1>Mkee Quiz</h1>
+        </header>
+    )
+}
+export default Header
